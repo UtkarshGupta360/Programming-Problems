b5b3d0f5c5ad562294f12e954e53b3c52d32d187
Utkarsh Gupta <83241246+UtkarshGupta360@users.noreply.github.com>
2022-08-24 03:50:46 +0530

    Create 0-1 Knapsack with O(N) SC.cpp

---
+++ 0-1 Knapsack with O(N) SC.cpp
@@ -0,0 +1,9 @@
+// Problem Link : https://www.interviewbit.com/problems/0-1-knapsack/
+int Solution::solve(vector<int> &A, vector<int> &B, int C) 
+{
+    int n = A.size() + 1;
+    int *dp = new int[C + 1];
+    for(int i = 0; i < C + 1; i++)dp[i]  = 0;
+    for(int i = 0; i < n - 1; i++) for(int j = C;j >= B[i]; j--) if(B[i] <= j) dp[j] = max(dp[j - B[i]] + A[i], dp[j]);
+    return dp[C];
+}
