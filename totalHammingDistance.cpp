42e6ade44159594d10210765b7459ddd0470e16a
Utkarsh Gupta <83241246+UtkarshGupta360@users.noreply.github.com>
2023-05-24 13:46:05 +0530

    Update totalHammingDistance.cpp

--- totalHammingDistance.cpp
+++ totalHammingDistance.cpp
@@ -1,3 +1,5 @@
+'''The Hamming distance between two integers is the number of positions at which the corresponding bits are different. Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums. '''
+'''Problem Link: https://leetcode.com/problems/total-hamming-distance/description/'''
 int totalHammingDistance(vector<int>& ans) 
     {
         vector<int> one(32,0);
